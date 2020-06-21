766b57b056b33a2d820d573ba598b56137f7fe5e
Aditya <mailingadityavs@gmail.com>
2020-06-21 19:45:11 +0530

    Added all comments.

--- wikiMarklet.js
+++ wikiMarklet.js
@@ -1,30 +1,34 @@
 // Console like operation
 
+// Selecting main div.
 content = document.getElementsByClassName("mw-parser-output")[0]
 divs = content.getElementsByTagName("div")
-for (i = divs.length-1; i >= 0; i--) {
+for (i = divs.length - 1; i >= 0; i--) {
     divs[i].remove()
 }
 
-// Remove [] from content.innerHTML
+// Remove []annotations from the page
 sups = content.getElementsByTagName("sup")
 for (i = sups.length - 1; i >= 0; i--) {
     sups[i].remove()
 }
 
+// Replace a tags with plain words.
 ats = content.getElementsByTagName("a")
 for (i = ats.length - 1; i >= 0; i--) {
     ats[i].replaceWith(ats[i].textContent)
 }
 
-// Add some basic css to it.
-// Maybe change colors?
 
+// Set body to only main content
 document.body.innerHTML = content.innerHTML
+// Set basic css padding
 document.body.style.padding = "10px";
 
-// Add same lines to the bookmarklet too.
-(function() {
+// TODO Maybe change colors later on?
+
+// Code in the form to be entered in Address Bar.
+(function () {
     content = document.getElementsByClassName("mw-parser-output")[0];
     divs = content.getElementsByTagName("div");
     for (i = divs.length - 1; i >= 0; i--) {
@@ -43,4 +47,6 @@ document.body.style.padding = "10px";
 
 })();
 
+// EXACT code to be entered in the Address bar.
 javascript: (function () { content = document.getElementsByClassName("mw-parser-output")[0]; divs = content.getElementsByTagName("div"); for (i = divs.length - 1; i >= 0; i--) { divs[i].remove(); } sups = content.getElementsByTagName("sup"); for (i = sups.length - 1; i >= 0; i--) { sups[i].remove(); } ats = content.getElementsByTagName("a"); for (i = ats.length - 1; i >= 0; i--) { ats[i].replaceWith(ats[i].textContent); } document.body.innerHTML = content.innerHTML; document.body.style.padding = "10px"; })();
+
